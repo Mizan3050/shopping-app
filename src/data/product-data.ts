4e6154092a0a31f56d9508eee247ab100329ca07
Mizan3050 <mizansid888@gmail.com>
2021-08-08 10:54:28 +0530

    data added

--- src/data/product-data.ts
+++ src/data/product-data.ts
@@ -2,34 +2,34 @@ export const ListOfProducts = {
     products: [
         {
             id: '1',
-            name: 'Canon 1200D',
+            name: 'Canon EOS 6D Mark II DSLR ',
             price: 42000,
             category: 'one',
-            imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+            imageUrl: 'https://rukminim1.flixcart.com/image/416/416/j62hrww0/dslr-camera/f/4/g/eos-6d-mark-ii-eos-6d-mark-ii-canon-original-imaewgyegmb88rkn.jpeg?q=70',
             quantity: 0
         },
         {
             id: '2',
-            name: 'Canon 1202D',
+            name: 'Canon EOS 200D II DSLR',
             price: 42222,
             category: 'two',
-            imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+            imageUrl: 'https://rukminim1.flixcart.com/image/416/416/camera/r/q/s/canon-eos-1200d-kit-ef-s18-55-is-ii-dslr-original-imaey5h3uxcbgbwy.jpeg?q=70',
             quantity: 0
         },
         {
             id: '3',
-            name: 'Canon 1203D',
+            name: 'Canon EOS 3000D DSLR',
             price: 42333,
             category: 'three',
-            imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+            imageUrl: 'https://rukminim1.flixcart.com/image/416/416/juwzf680/dslr-camera/g/a/q/200d-ii-200d-ii-canon-original-imaffvrhzyqzayys.jpeg?q=70',
             quantity: 0
         },
         {
             id: '4',
-            name: 'Canon 1204D',
+            name: 'Canon EOS 80D DSLR',
             price: 42444,
             category: 'four',
-            imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+            imageUrl: 'https://rukminim1.flixcart.com/image/416/416/jefzonk0/dslr-camera/g/v/r/eos-80d-80d-canon-original-imaf329wfqqyawwx.jpeg?q=70',
             quantity: 0
         }
     ]
